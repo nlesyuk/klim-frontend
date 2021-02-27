8b9fa3571f6a615deef1d108832d707a96aba87a
Nazar Lesyuk <pronaza@gmail.com>
2021-02-28 00:20:12 +0200

    Dropdown submenu refreshed in main enu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,89 +25,17 @@
       <img src="@/assets/logo.png" alt="logo" />
     </a>
 
-    <Menu class="header__nav" />
-
-    <!--
-    <nav class="header__nav">
-      <ul>
-        <li>
-          <router-link
-            exact-path
-            exact-active-class="active"
-            to="/"
-            class="header__nav-item"
-            >Works</router-link
-          >
-        </li>
-        <li>
-          <router-link
-            exact-path
-            exact-active-class="active"
-            to="/shots"
-            class="header__nav-item main"
-            >Shots</router-link
-          >
-          <div class="header__dropdown">
-            <router-link
-              exact
-              exact-active-class="active"
-              :to="{ path: 'shots', query: { filter: 'all' } }"
-              class="header__nav-subitem"
-              >all</router-link
-            >
-            <router-link
-              exact-path
-              exact-active-class="active"
-              :to="{ path: 'shots', query: { filter: 'portrait' } }"
-              class="header__nav-subitem"
-              >portrait</router-link
-            >
-            <router-link
-              exact-path
-              exact-active-class="active"
-              :to="{ path: 'shots', query: { filter: 'landscape' } }"
-              class="header__nav-subitem"
-              >landscape</router-link
-            >
-            <router-link
-              exact-path
-              exact-active-class="active"
-              :to="{ path: 'shots', query: { filter: 'mood' } }"
-              class="header__nav-subitem"
-              >mood</router-link
-            >
-          </div>
-        </li>
-        <li>
-          <router-link
-            exact-path
-            exact-active-class="active"
-            to="/photo"
-            class="header__nav-item"
-            >Photo</router-link
-          >
-        </li>
-        <li>
-          <router-link
-            exact-path
-            exact-active-class="active"
-            to="/contact"
-            class="header__nav-item"
-            >Contact</router-link
-          >
-        </li>
-      </ul>
-    </nav> -->
+    <Nav class="header__nav" />
   </header>
 </template>
 
 <script lang="ts">
-import Menu from "./menu.vue";
+import Nav from "./Nav.vue";
 
 export default {
   name: "Header",
   components: {
-    Menu
+    Nav
   }
 };
 </script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,83 @@
+<template>
+  <vsm-menu
+    :menu="menu"
+    :base-width="380"
+    :screen-offset="10"
+    element="div"
+    handler="hover"
+  >
+    <template #default="data">
+      <component :is="data.item.content" class="content" />
+    </template>
+    <template #title="data">
+      {{ data.item.title }}
+    </template>
+    <template #after-nav>
+      <!--Mobile Burger, buttons, etc-->
+      <vsm-mob>Mobile Content</vsm-mob>
+    </template>
+  </vsm-menu>
+</template>
+
+<script>
+import ShotsSubmenu from "./dropdowns/ShotsSubmenu.vue";
+
+export default {
+  data() {
+    return {
+      menu: [
+        // 1
+        {
+          title: "Works",
+          attributes: {
+            class: ["header__nav-item"]
+          },
+          listeners: {
+            click: () => {
+              this.$router.push("/");
+            }
+          }
+        },
+        // 2
+        {
+          title: "Shots",
+          attributes: {
+            class: ["header__nav-item"]
+          },
+          listeners: {
+            click: () => {
+              this.$router.push("/shots");
+            }
+          },
+          dropdown: "ShotsSubmenu",
+          content: ShotsSubmenu
+        },
+        // 3
+        {
+          title: "Photo",
+          attributes: {
+            class: ["header__nav-item"]
+          },
+          listeners: {
+            click: () => {
+              this.$router.push("/photo");
+            }
+          }
+        },
+        // 4
+        {
+          title: "Contact",
+          attributes: {
+            class: ["header__nav-item"]
+          },
+          listeners: {
+            click: () => {
+              this.$router.push("/contact");
+            }
+          }
+        }
+      ]
+    };
+  }
+};
+</script>
