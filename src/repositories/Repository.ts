f06dcf96deca038c6580792a3136476f9fa63045
Nazar Lesyuk <pronaza@gmail.com>
2021-04-11 22:48:20 +0300

    New base URL

--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseDomain = " http://localhost:3000";
+// const baseDomain = "http://localhost:3000";
+// const baseDomain = "http://localhost:8090";
+const baseDomain = "http://138.68.74.145";
 
 const baseURL = `${baseDomain}`;
 
