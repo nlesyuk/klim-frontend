36da12ee5f902cc32871605111c704cd6e139fa2
Nazar Lesyuk <nazar.lesyuk@logist-pro.com>
2021-04-15 19:23:05 +0300

    Preview work: click on work section for get a work page

--- src/components/WorkPreview.vue
+++ src/components/WorkPreview.vue
@@ -1,11 +1,13 @@
 <template>
   <figure class="work">
-    <img :src="work.preview_img" :alt="work.title" class="work__img" />
-    <router-link class="work__title" :to="{ path: '/work/' + work.id }">
-      <svg width="24" height="24">
-        <use xlink:href="#svg-sprite--video"></use>
-      </svg>
-      {{ work.title }}
+    <router-link class="work__cont" :to="{ path: '/work/' + work.id }">
+      <img :src="work.preview_img" :alt="work.title" class="work__img" />
+      <div class="work__description">
+        <svg width="24" height="24">
+          <use xlink:href="#svg-sprite--video"></use>
+        </svg>
+        <h2 class="work__title">{{ work.title }}</h2>
+      </div>
     </router-link>
   </figure>
 </template>
