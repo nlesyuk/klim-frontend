fe7809a0965e290f9bbdc51d589d77810735fdc2
Nazar Lesyuk <pronaza@gmail.com>
2021-04-18 18:09:58 +0300

    Note: CRUD model for build repository

--- src/repositories/VideosRepository.ts
+++ src/repositories/VideosRepository.ts
@@ -2,6 +2,7 @@ import Repository from "./Repository";
 
 const resources = "videos";
 
+// use CRUD model for build repository
 export default {
   getAllVideos() {
     return Repository.get(`${resources}`);
@@ -9,4 +10,13 @@ export default {
   getVideo(id) {
     return Repository.get(`${resources}/${id}`);
   }
+  // create(payload) {
+  //   return Repository.post(`${resources}`, payload);
+  // },
+  // update(payload, id) {
+  //   return Repository.put(`${resources}/${id}`, payload);
+  // },
+  // delete(id) {
+  //   return Repository.delete(`${resources}/${id}`);
+  // }
 };
