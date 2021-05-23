a91f1227eb51c53891c8fbd89e69cf09a302e68e
Nazar Lesyuk <pronaza@gmail.com>
2021-05-23 22:13:21 +0300

    env manage stage by name (dev, prod, devjson)

--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const baseDomain =
-  +process.env.VUE_APP_SERVER_ENVIRONMENT === 1
+  process.env.VUE_APP_SERVER_ENVIRONMENT === "prod"
     ? process.env.VUE_APP_SERVER_PRODUCTION
-    : +process.env.VUE_APP_SERVER_ENVIRONMENT === 2
+    : process.env.VUE_APP_SERVER_ENVIRONMENT === "dev"
     ? process.env.VUE_APP_SERVER_DEV
     : process.env.VUE_APP_SERVER_DEV_JSON_SERVER;
 
