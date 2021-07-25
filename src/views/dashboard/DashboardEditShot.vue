8616869ec7896bd46e72c0ce3ac50c4535e7f4c8
Nazar Lesyuk <pronaza@gmail.com>
2021-07-25 22:36:41 +0300

    DashboardShot: edit shot - done

---
+++ src/views/dashboard/DashboardEditShot.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <ul class="dashboard__list-imgs" v-if="shot">
+      <li>
+        <span class="dashboard__badge badge-yellow">id: {{ shot.id }}</span>
+        <div v-if="shot.workId" class="dashboard__badge badge-green">
+          Linked to post-id: {{ shot.workId }}
+        </div>
+
+        <img :src="shot.src" alt="preview" />
+
+        <div class="dashboard__select">
+          <select v-model="shot.workId">
+            <option disabled selected value="null">
+              Please linking the Shot to the Work
+            </option>
+            <option v-for="(item, idx) of videos" :key="idx" :value="item.id">
+              {{ item.title }}
+            </option>
+          </select>
+        </div>
+
+        <p class="dashboard__text">Please choose category for this shot:</p>
+        <label
+          class="dashboard__label mb0"
+          v-for="(category, idx) of categories"
+          :key="idx"
+        >
+          <template v-if="!['all'].includes(category)">
+            <input
+              type="checkbox"
+              v-model="selectedCategories"
+              :value="category"
+            />
+            <span class="inline">{{ category }}</span>
+          </template>
+        </label>
+
+        <button type="button" @click="update" class="dashboard__btn">
+          Update shot
+        </button>
+        <button type="button" @click="close" class="dashboard__btn">
+          Close
+        </button>
+      </li>
+    </ul>
+    <p v-else class="dashboard__badge badge-red">Something went wrong</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    shot: {
+      type: Object,
+      required: true
+    },
+    videos: {
+      required: true
+    },
+    categories: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      selectedCategories: []
+    };
+  },
+  watch: {
+    shot() {
+      this.updateSelectedCategories();
+    }
+  },
+  methods: {
+    update() {
+      const { id, src, workId } = this.shot;
+      const payload = {
+        id,
+        src,
+        workId,
+        category: this.selectedCategories
+      };
+      this.$emit("updated", payload);
+    },
+    close() {
+      this.selectedCategories = [];
+      this.$emit("close");
+    },
+    updateSelectedCategories() {
+      this.selectedCategories = [];
+      this.selectedCategories = [...this.shot.category];
+    }
+  },
+  mounted() {
+    this.updateSelectedCategories();
+  }
+};
+</script>
--- src/views/dashboard/DashboardShots.vue
+++ src/views/dashboard/DashboardShots.vue
@@ -8,6 +8,16 @@
       Add shot
     </button>
     <AddShot v-if="isShowAddShot" />
+
+    <EditShot
+      v-if="isEdit"
+      :shot="editedShot"
+      :videos="videos"
+      :categories="categories"
+      @updated="update"
+      @close="closeEdit"
+    />
+
     <button type="button" @click="refresh" class="dashboard__btn">
       Refresh shots
     </button>
@@ -25,42 +35,54 @@
 
 <script>
 import AddShot from "./DashboardAddShot.vue";
+import EditShot from "./DashboardEditShot.vue";
 import GridPhotosShots from "../../components/GridPhotosShots";
 import { mapState, mapActions } from "vuex";
 import { RepositoryFactory } from "Repositories/RepositoryFactory.ts";
-const VideosRepository = RepositoryFactory.get("videos");
+const ShotRepository = RepositoryFactory.get("shots");
 
 export default {
   components: {
     GridPhotosShots,
-    AddShot
+    AddShot,
+    EditShot
   },
   data() {
     return {
+      isEdit: false,
+      editedShot: null,
       isShowAddShot: false,
       filteredPhotos: []
     };
   },
   computed: {
     ...mapState({
+      videos: state => state.videos.videos,
+      categories: state => state.shots.category,
       allPhotos: state => state.shots.shots
     })
   },
   methods: {
-    ...mapActions(["getAllShots"]),
+    ...mapActions(["getAllShots", "getAllVideos"]),
     refresh() {
       this.getAllShots();
     },
     remove(id) {
       this.filteredPhotos = this.filteredPhotos.filter(v => v.id != id);
-      console.log("removeImg", id);
 
       VideosRepository.delete(id);
     },
     edit(id) {
-      console.log("editImg", id);
-      //
-      // VideosRepository.update(payload, id);
+      this.isEdit = true;
+      const finded = this.filteredPhotos.filter(v => v.id === id);
+      this.editedShot = finded?.length ? finded[0] : null;
+    },
+    update(payload) {
+      // console.log("update", payload);
+      ShotRepository.update(payload, payload.id);
+    },
+    closeEdit() {
+      this.isEdit = false;
     }
   },
   mounted() {
@@ -70,6 +92,10 @@ export default {
       });
     }
     this.filteredPhotos = this.allPhotos;
+
+    if (!this.videos) {
+      this.getAllVideos();
+    }
   }
 };
 </script>
