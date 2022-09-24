c473c88629dacb796e16f572e397dce4e2195864
Nazar Lesyuk <pronaza@gmail.com>
2022-09-24 18:44:04 +0300

    Extend constants for reuse it in other part of project

--- src/helper/constants.ts
+++ src/helper/constants.ts
@@ -1,4 +1,39 @@
-export const menu = ["slider", "works", "shots", "photos", "contacts"];
+// general
+export const domain = process.env.VUE_APP_DOMAIN || "klimstepan.com";
+
+export const isCinematographerMode = domain === "klimstepan.com";
+
+// server
+export const serverDomain =
+  process.env.VUE_APP_SERVER_ENVIRONMENT === "prod"
+    ? process.env.VUE_APP_SERVER_PRODUCTION
+    : process.env.VUE_APP_SERVER_DEV;
+
+export const apiVersion = `api/v1`;
+
+export const APIURL = `${serverDomain}/${apiVersion}`;
+
+// front end
+const mainMenu = [
+  { name: "slider", isSinematographer: true, isPhotographer: true },
+  { name: "works", isSinematographer: true, isPhotographer: false },
+  { name: "shots", isSinematographer: true, isPhotographer: false },
+  { name: "photos", isSinematographer: true, isPhotographer: true },
+  { name: "contacts", isSinematographer: true, isPhotographer: true }
+];
+
+export const menuSinematographer = mainMenu
+  .filter(v => v.isSinematographer)
+  .map(v => v.name);
+
+export const menuPhotographer = mainMenu
+  .filter(v => v.isPhotographer)
+  .map(v => v.name);
+
+export const menu = isCinematographerMode
+  ? menuSinematographer
+  : menuPhotographer;
+
 export const cinematographerCategories = [
   "all",
   "automotive",
@@ -8,8 +43,13 @@ export const cinematographerCategories = [
 ];
 
 export const photographerCategories = [
+  "all",
   "automotive",
   "fashion",
   "lifestyle",
   "personal"
 ];
+
+export const categories = isCinematographerMode
+  ? cinematographerCategories
+  : photographerCategories;
